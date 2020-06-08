a34cacca2b1e2789abcbc3421121edc061b6f3a9
fisamodo <fisamodo@vsite.hr>
2020-06-08 07:38:22 -0700

    fixed navbar

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ $('#view-work').on('click', function(){
         scrollTop: images
     }, 900);
 });
+
+$('#view-mimice').on('click', function(){
+    const images = $('#mimice').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 1600);
+});
+
+$('#view-poljica').on('click', function(){
+    const images = $('#poljica').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 2300);
+});
